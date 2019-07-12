2b686dfd0f926fd0787d82de44a0fa74d4af8832
Petr Ryšavý <petr.rysavy@fel.cvut.cz>
2019-07-12 09:54:01 +0200

    Random individual #13

---
+++ operators.py
@@ -0,0 +1,15 @@
+import numpy as np
+import networkx as nx
+import copy
+
+np.random.seed(42)
+
+
+'''
+Takes as input a graph structure and returns a copy of the graph with random weights.
+'''
+def random_individual(graph: nx.Graph) -> nx.Graph:
+    random_graph = copy.deepcopy(graph)
+    for (u,v) in random_graph.edges:
+        random_graph[u][v]['weight'] = np.random.random()
+    return random_graph
