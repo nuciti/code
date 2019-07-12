e6f56a44171a2781677284e1ba824fd6542ed7f2
Petr Ryšavý <petr.rysavy@fel.cvut.cz>
2019-07-12 10:22:35 +0200

    Crossover operator #13

--- operators.py
+++ operators.py
@@ -18,3 +18,15 @@ def random_individual(graph):
 
 def graph_weights(graph):
     return [graph.edges[x]['weight'] for x in graph.edges] 
+
+
+'''
+Does the crossover operation. Takes as input two graphs with weights and produces a new graph with weights. Each weight
+is randomly and independently picked from either of the two original graphs.
+'''
+def crossover(graph1, graph2):
+    new_graph = copy.deepcopy(graph1)
+    for (u, v) in new_graph.edges:
+        if np.random.choice([True, False]):
+            new_graph[u][v]['weight'] = graph2[u][v]['weight']
+    return new_graph
